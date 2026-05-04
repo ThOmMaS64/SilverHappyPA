69f88b05528d2ef732cee65e0b7439878868e63a
Ali-Jahmi <ali.jahmi@hotmail.com>
2026-05-04 23:17:19 +0200

    update(front):correction bug nouvelle discussion créée à chaque demande de service

--- API/handlersFront/OpenOrCreateDiscussion.go
+++ API/handlersFront/OpenOrCreateDiscussion.go
@@ -28,29 +28,32 @@ func OpenOrCreateDiscussion(database *sql.DB) http.HandlerFunc {
 
 		var idDiscussion int
 
-		rowDiscussion := database.QueryRow("SELECT ID_DISCUSSION FROM DISCUSSION WHERE ID_SERVICE = ? AND ((user1_id = ? AND user2_id = ?) OR (user1_id = ? AND user2_id = ?))", idService, id, idUserServiceProvider, idUserServiceProvider, id)
-
+		rowDiscussion := database.QueryRow("SELECT ID_DISCUSSION FROM DISCUSSION WHERE (user1_id = ? AND user2_id = ?) OR (user1_id = ? AND user2_id = ?) LIMIT 1", id, idUserServiceProvider, idUserServiceProvider, id)
 		err = rowDiscussion.Scan(&idDiscussion)
 
 		if err == sql.ErrNoRows {
-			res, insertErr := database.Exec("INSERT INTO DISCUSSION (user1_id, user2_id, ID_SERVICE) VALUES (?, ?, ?)", id, idUserServiceProvider, idService)
 
+			res, insertErr := database.Exec("INSERT INTO DISCUSSION (user1_id, user2_id, ID_SERVICE) VALUES (?, ?, ?)", id, idUserServiceProvider, idService)
+			
 			if insertErr != nil {
+
 				http.Redirect(w, r, "http://localhost/ProjetAnnuel/services.php?error=system3", 303)
 				return
 			}
 
 			newID, _ := res.LastInsertId()
 			idDiscussion = int(newID)
+			
+		} else if err == nil {
 
-		} else if err != nil {
-			http.Redirect(w, r, "http://localhost/ProjetAnnuel/services.php?error=system4", 303)
-			return
+			database.Exec("UPDATE DISCUSSION SET ID_SERVICE = ? WHERE ID_DISCUSSION = ?", idService, idDiscussion)
+
 		}
 
 		url := fmt.Sprintf("http://localhost/ProjetAnnuel/messaging.php?id_discussion=%d&id_service=%s&info=from_services", idDiscussion, idService)
 		http.Redirect(w, r, url, 303)
-		 
+		return
+ 
 	}
 
 }
